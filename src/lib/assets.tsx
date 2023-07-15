642e0e80e58afa36c0767813b870f05372555717
age2pierre <pierre.j.robillard@gmail.com>
2023-07-15 22:18:06 +0200

    feat: add meshNames prop to MeshAsset

--- src/lib/assets.tsx
+++ src/lib/assets.tsx
@@ -1,20 +1,30 @@
 import { createMemo, createResource, onCleanup } from 'solid-js'
 import type metadata from '../assets/metadata'
 import { useBabylon } from './useBabylon'
-import { AbstractMesh } from '@babylonjs/core'
+import { AbstractMesh, Node } from '@babylonjs/core'
 
 export type AssetMetadata = typeof metadata
 export type AssetFileName = keyof AssetMetadata
 
-export function MeshAsset(props: { fileName: AssetFileName }) {
+export function MeshAsset<F extends AssetFileName>(props: {
+  fileName: F
+  meshNames?: Array<AssetMetadata[F]['meshes'][number]>
+}) {
   const { scene, getAsset } = useBabylon()
 
   const [instancedRoots] = createResource(
     () => props.fileName,
     async (file) => {
       const container = await getAsset(file)
-      const entries = container.instantiateModelsToScene()
-
+      const entries = container.instantiateModelsToScene(undefined, undefined, {
+        predicate: (entity) => {
+          if (!props.meshNames) return true
+          return (
+            entity instanceof Node &&
+            props.meshNames.includes(entity.name as any)
+          )
+        },
+      })
       return entries.rootNodes
     },
   )
