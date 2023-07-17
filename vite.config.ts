1eddba7366e105419871fdd1ed306aea75b62283
age2pierre <pierre.j.robillard@gmail.com>
2023-07-17 17:29:39 +0200

    chore: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import checker from 'vite-plugin-checker'
 import { splitVendorChunkPlugin } from 'vite'
 
 export default defineConfig(async (mode) => ({
+  base: '/solid-akkadi/',
   plugins: [
     splitVendorChunkPlugin(),
     solidPlugin({}),
