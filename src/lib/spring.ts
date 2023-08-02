68db460abfbec8344f8b7352c4ccedf25f95dbf0
age2pierre <pierre.j.robillard@gmail.com>
2023-08-02 19:06:11 +0200

    feat: add 2 spring presets

--- src/lib/spring.ts
+++ src/lib/spring.ts
@@ -235,4 +235,6 @@ export const PRESETS = {
   gentle: { stiffness: 120, damping: 14 },
   wobbly: { stiffness: 180, damping: 12 },
   stiff: { stiffness: 210, damping: 20 },
+  slow: { stiffness: 280, damping: 60 },
+  molasses: { stiffness: 280, damping: 120 },
 } as const satisfies Record<string, SpringOpts>
